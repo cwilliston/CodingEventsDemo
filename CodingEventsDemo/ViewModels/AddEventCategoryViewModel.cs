fa59b2ba5ef96f6d7ecbdad2c6974b7ca0fe2269
cwilliston <carolynnwilliston@gmail.com>
2022-11-09 20:20:45 -0600

    Ch 17 Studio complete

---
+++ CodingEventsDemo/ViewModels/AddEventCategoryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CodingEventsDemo.ViewModels
+{
+    public class AddEventCategoryViewModel
+    {
+        [StringLength(20, MinimumLength = 3)]
+        public string Name { get; set; }
+
+
+
+    }
+}
